91766c9b687dcdc671769a37b34ba15a45dc0274
EricoBruner <ericodasilvabruner@gmail.com>
2023-07-29 23:13:51 -0300

    ajustado schema do joi para não aceitar letras no campo cpf

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -3,6 +3,9 @@ import joi from "joi";
 export const customerSchema = joi.object({
   name: joi.string().required(),
   phone: joi.string().min(10).max(11).required(),
-  cpf: joi.string().min(11).max(11).required(),
+  cpf: joi
+    .string()
+    .length(11)
+    .pattern(/^\d{11}$/),
   birthday: joi.date().required(),
 });
